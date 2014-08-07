728dfd7ffce4c59c04b28cb75c2eaad42f64b2f0
Patrick Steiner <patrick@steiner-buchholz.de>
2014-08-07 14:34:58 +0200

    update

--- src/main/java/com/redhat/demo/heisedemo/HeiseDemoBatchStarter/SingleContractRequestStarterBean.java
+++ src/main/java/com/redhat/demo/heisedemo/HeiseDemoBatchStarter/SingleContractRequestStarterBean.java
@@ -2,8 +2,14 @@ package com.redhat.demo.heisedemo.HeiseDemoBatchStarter;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.manager.RuntimeEngine;
 import org.kie.services.client.api.RemoteRestRuntimeFactory;
+import org.kie.api.runtime.process.ProcessInstance;
+
 import org.switchyard.component.bean.Service;
 
 @Service(SingleContractRequestStarter.class)
@@ -24,24 +30,19 @@ public class SingleContractRequestStarterBean implements
 			restSessionFactory = new RemoteRestRuntimeFactory("com.redhat.demo:HeiseDemo:1.4", // this can be viewed from business-central -> properties of deployed Process
 															new URL("http://localhost:8280/business-central"), 
 																	"psteiner", "change12_me");
-//			
-//			// create REST request
+			
+			// create REST request
 //			RuntimeEngine engine = restSessionFactory.newRuntimeEngine();
 //			KieSession ksession = engine.getKieSession();
-//	
-//			Map<String, Object> params = new HashMap<String, Object>();
-//	
-//			params.put("personendaten", request.getPerson());
-//			params.put("versichertesObjekt", request.getObject());
-//			params.put("vertrag", request.getContract());
-//			
-//			System.out.println(" ..........  foo 2...............");
-//			System.out.println(request.getPerson().getVorname());
-//			System.out.println(" ..........  foo 3...............");
-//			
-//			
-////			ProcessInstance processInstance = ksession.startProcess("HeiseDemo.Hausrat", params);
-//		
+	
+			Map<String, Object> params = new HashMap<String, Object>();
+	
+			params.put("personendaten", request.getPerson());
+			params.put("versichertesObjekt", request.getObject());
+			params.put("vertrag", request.getContract());			
+
+//			ProcessInstance processInstance = ksession.startProcess("HeiseDemo.Hausrat", params);
+		
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
